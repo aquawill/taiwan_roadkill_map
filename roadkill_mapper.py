40521cb840d74b61986f5623506bb30bd5894dda
ww <aquawill@msn.com>
2020-12-08 15:46:33 +0800

    Update Python and JS codes.

--- roadkill_mapper.py
+++ roadkill_mapper.py
@@ -12,11 +12,13 @@ mode = 'fastest;car;traffic:disabled'  # Routing Mode of HERE Routing API.
 # Output the list of geo-coordinates of road segments for mapping.
 def get_route(ori_lat, ori_lon, dest_lat, dest_lon, route_mode):
     route_url = 'https://route.api.here.com/routing/7.2/calculateroute.json?'
-    wp0 = ('{},{}'.format(ori_lat, ori_lon))  # Starting Point of road segment
-    wp1 = ('{},{}'.format(dest_lat, dest_lon))  # End point of road segment
+    wp0 = '{},{}'.format(ori_lat, ori_lon).replace(' ', '')  # Starting Point of road segment
+    wp1 = '{},{}'.format(dest_lat, dest_lon).replace(' ', '')  # End point of road segment
     route_options = '&mode={}&legAttributes=shape'.format(route_mode)
     url = route_url + 'app_id=' + app_id + '&app_code=' + app_code + '&waypoint0=geo!' + wp0 + '&waypoint1=geo!' + wp1 + route_options
+    print(url)
     json_result = json.loads(requests.get(url).text)
+    print(json_result)
     routes = json_result['response']['route']
     route_shape = []
     for route in routes:
@@ -39,9 +41,8 @@ full_list = []
 # Open input csv file, column names of raw input are:
 # id,species,type,recommended_type,season,day_night,lat_1,lng_1,lat_2,lng_2,route_type,route_number,route_desc,recommended_voice
 # Add one more column "wkt" to store the geometries of roadkill road segments in WKT format.
-with open('roadkill_locations.csv', newline='') as csv_file:
-    dict_reader = csv.DictReader(csv_file, delimiter=',')
-
+with open('roadkill_locations.csv', encoding='utf-8') as csv_file:
+    dict_reader = csv.DictReader(csv_file, delimiter='\t')
     fieldnames = dict_reader.fieldnames
     fieldnames.append('wkt')
 
@@ -71,7 +72,7 @@ with open('roadkill_locations.csv', newline='') as csv_file:
         full_list.append(row)
 
 # Write the result to new csv file.
-with open('roadkill_locations_new.csv', mode='w', newline='') as csv_output_file:
-    writer = csv.DictWriter(csv_output_file, fieldnames=dict_reader.fieldnames)
+with open('roadkill_locations_output.csv', mode='w', newline='', encoding='utf-8') as csv_output_file:
+    writer = csv.DictWriter(csv_output_file, delimiter='\t', fieldnames=dict_reader.fieldnames)
     writer.writeheader()
     writer.writerows(full_list)
